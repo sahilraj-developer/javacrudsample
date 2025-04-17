0e869a5a911c7a3c479aab932c49fee5076e4c13
sahil raj <sahil27raj@gmail.com>
2025-04-17 15:43:26 +0530

    add the changes for update and delete features

--- src/main/java/com/sahil/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/sahil/journalApp/controller/JournalEntryController.java
@@ -7,8 +7,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +30,22 @@ public class JournalEntryController {
     public boolean create(@RequestBody JournalEntry myEntry){
         journalEntries.put(myEntry.getId(),myEntry);
         return true;
+    }
+
+    @GetMapping("/abc/{id}")
+    public JournalEntry getJournalEntryById(@PathVariable long id){
+       return  journalEntries.get(id);
+    }
+
+    @DeleteMapping("/abc/{id}")
+    public JournalEntry deleteJournalEntryById(@PathVariable long id){
+       return  journalEntries.remove(id);
+    }
+
+
+    @PutMapping("/abc/{id}")
+    public JournalEntry deleteJournalEntryById(@PathVariable long id,@RequestBody JournalEntry myEntry){
+       return  journalEntries.put(id,myEntry);
     }
     
 }
