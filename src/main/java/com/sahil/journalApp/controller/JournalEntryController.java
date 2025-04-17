da427a7794dcedd776d0da798929176457e15772
sahil raj <sahil27raj@gmail.com>
2025-04-17 13:55:20 +0530

    add the changes for crud

---
+++ src/main/java/com/sahil/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,32 @@
+package com.sahil.journalApp.controller;
+
+import com.sahil.journalApp.entity.JournalEntry;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+    private Map<Long, JournalEntry> journalEntries  = new HashMap<>();
+   
+    @GetMapping("/abc")
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping("/abc")
+    public boolean create(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(),myEntry);
+        return true;
+    }
+    
+}
